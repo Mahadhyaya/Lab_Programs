4911db8fabb86f2f95872b9259e70c9ac1cfed26
Soorya_Kumar_M_H <79861274+Mahadhyaya@users.noreply.github.com>
2022-02-25 21:52:29 +0530

    Updated

--- 3a.sh
+++ 3a.sh
@@ -8,9 +8,12 @@ com
 if [ -e $1 ]
 then
 set -- `ls -ld $1`
-echo "Permission are $1"
-echo "File link counts $2:"
-echo "File Modification is done on $6 $7 and $8"
+echo "Permission are: $1"
+echo "File link counts: $2"
+echo "User: $3"
+echo "Group User:$4"
+echo "File Modification is done on $5 $6 $7 at $8"
+echo "File name: $9"
 else
 echo "File does not exits"
 fi
