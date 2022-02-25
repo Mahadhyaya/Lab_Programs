662eac1c16225cb2795e165839d609296dbf2f31
Soorya_Kumar_M_H <79861274+Mahadhyaya@users.noreply.github.com>
2022-02-25 21:47:53 +0530

    Updated

--- 2a.sh
+++ 2a.sh
@@ -6,27 +6,13 @@ otherwise output each filename followed by its permissions.
 
 com
 
-echo -n "Enter first filename:"
-read file1
-if [ -e $file1 ]
+if [ -e $1 ] && [ -e $2 ]
 then
 set -- `ls -ld $file1`
 file1perm=$1
-else
-echo "File doesnt exit"
-exit
-fi
-echo -n "Enter Second filename:"
-read file2
-if [ -e $file2 ]
-then
 set -- `ls -ld $file2`
 file2perm=$1
-else
-echo "File doesnt exit"
-exit
-fi
-if [ $file1perm = $file2perm ]
+if [ "$file1perm" = "$file2perm" ]
 then
 echo "File Permission are identical"
 echo "Permission is $file1perm"
@@ -34,4 +20,5 @@ else
 echo "File Permission are not identical"
 echo "$file1 Permission is $file1perm"
 echo "$file2 Permission is $file2perm"
+fi
 fi
