84b490fd71a7224107062eb7924373b7e892eaf3
Soorya_Kumar_M_H <79861274+Mahadhyaya@users.noreply.github.com>
2022-05-14 13:13:21 +0530

    8b.sh

--- 8b.sh
+++ 8b.sh
@@ -1,49 +1,19 @@
-echo -n "Enter the username:"
-read userName
-temporary=`grep -wi "$username" /etc/passwd`
-if [ "$userName" = `whoami` ]
+if [ $# -eq 1 ]
 then
-	echo "user already logged in"
-	exit
-fi
-if [ $? -eq 0 ]
-then 
-	
-	
-	echo "Ram"
-	echo -n "mention the time it should wait for login in minutes:"
-	read waitingTime
-	temp=`echo $waitingTime | grep ^[0-9]$`
+	var=`grep -owi $1 /etc/passwd`
 	if [ $? -eq 0 ]
 	then
-		currentHour=`date +%H`
-		currentMinute=`date +%M`
-		totalMinute=`expr $currentMinute + $waitingTime`
-		totalHour=$currentHour
-		if [ $totalMinute -gt 60 ]
+		sleep 1m
+		success=`who | grep -wo "$var"`
+		if [ $? -eq 0 ]
 		then
-			totalMinute=`expr 60 - $totalMinute`
-			totalHour=`expr $currentHour + 1`
+			echo "Login Success"
+		else 
+			echo "Login Not Success"
 		fi
-		while [ $totalMinute -ge $currentMinute -o $totalHour -ge $currentHour ]
-		do
-			if [ "$userName" = `whoami` ]
-			then
-				echo "user logged in..."
-				exit
-			fi
-		done
-		echo "user not logged in..."
-		exit
 	else
-		echo "Enter the crct time"
+		echo "User Doesn't Exists"
 	fi
-
-	
-	
-	
-	echo "Soorya"
-else
-	echo "User doesn't exists"
+else 
+	echo "Please provide 1 argument"
 fi
-		
